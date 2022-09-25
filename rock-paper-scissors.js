7a86b2917f202734364fbfa5f8436e854a525fdf
Lewis Knapp <lknapp1888@gmail.com>
2022-09-25 17:11:39 +0100

    add one round version of rock paper scissors

---
+++ rock-paper-scissors.js
@@ -0,0 +1,38 @@
+function getComputerChoice() {
+    let choiceRefNum = Math.floor((Math.random() * 3));
+    if (choiceRefNum === 0) {
+        return 'rock';
+    }
+    else if (choiceRefNum === 1) {
+        return 'paper';
+    }
+    else if (choiceRefNum === 2) {
+        return 'scissors';
+    }
+}
+
+function playRound() {
+    let playerSelection = prompt("Make your selection by typing 'rock', 'paper', or 'scissors': ", "");
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = getComputerChoice();
+    if (((playerSelection === 'rock') && (computerSelection === 'scissors')) 
+    || ((playerSelection === 'paper') && (computerSelection === 'rock'))
+    || ((playerSelection === 'scissors') && (computerSelection === 'paper')))  {
+        return `You: ${playerSelection}
+Computer: ${computerSelection}
+You win!`;
+    }
+    else if (playerSelection === computerSelection) {
+        return `You: ${playerSelection}
+Computer: ${computerSelection}
+Draw!`;
+    }
+    else {
+        return `You: ${playerSelection}
+Computer: ${computerSelection}
+You lose!`;
+    }
+}
+
+console.log(playRound());
+
